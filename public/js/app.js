cc11db4d89c585146420d66b686b4d307906b36c
Cairo Flores <florescairo2902@gmail.com>
2020-10-21 11:22:13 -0700

    public configured

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+    render = () => {
+        return(
+            <div className="container">
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('main'))
