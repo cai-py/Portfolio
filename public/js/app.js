abda66d92afe0a418b1701966287670a2c18f94c
Cairo Flores <florescairo2902@gmail.com>
2020-10-25 07:47:12 -0700

    site formatted

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,11 @@ class App extends React.Component {
         page: null
     }
 
-    // componentDidMount = () => {
-    //     this.setState({
-    //         page: 'about'
-    //     })
-    // }
+    componentDidMount = () => {
+        this.setState({
+            page: 'about'
+        })
+    }
 
     about = () => {
         this.setState({
@@ -29,21 +29,24 @@ class App extends React.Component {
 
     render = () => {
       return (
-        <div className="container">
+        <div className="container w-11/12 max-w-6xl">
             <NavBar 
                 about={this.about}
                 projects={this.projects}
                 current={this.current}
             />
-            {(this.state.page == 'about')
-                ?<About />:null
-            }
-            {(this.state.page === 'projects')
-                ?<Projects />:null
-            }
-            {(this.state.page === 'current')
-                ?<Current />:null
-            }
+            <div className="page text-pallet-100 border border-pallet-1100 bg-pallet-900 mt-3">
+                {(this.state.page == 'about')
+                    ?<About />:null
+                }
+                {(this.state.page === 'projects')
+                    ?<Projects />:null
+                }
+                {(this.state.page === 'current')
+                    ?<Current />:null
+                }
+            </div>
+            
         </div>
       )
     }
@@ -52,9 +55,13 @@ class App extends React.Component {
 class NavBar extends React.Component {
     render = () => {
         return (
-            <nav className="flex justify-between items-center">
+            <nav className="navContainer
+                flex justify-between items-center 
+                border-2 border-pallet-200 
+                mt-4 px-2"
+                >
                 <div>
-                    <h1 className="font-bold text-blue-500 text-3xl">
+                    <h1 className="font-bold text-pallet-100 text-3xl">
                         <a href="/">Cairo Flores</a>
                     </h1>
                 </div>
@@ -77,8 +84,13 @@ class NavBar extends React.Component {
 class About extends React.Component {
     render = () => {
         return (
-            <div className="home-page">
-                <h1>About</h1>
+            <div className="home-page p-4">
+                <h1 className="text-2xl">About</h1>
+                <article>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec leo neque. Integer a tincidunt sem. Maecenas in dolor dictum, facilisis magna eget, tincidunt massa. Nam risus urna, volutpat eget tincidunt vel, congue ut diam. Phasellus eget nisi sagittis, suscipit tellus aliquet, rhoncus dui. Vestibulum sem lectus, dapibus sed dapibus in, ullamcorper vel orci. Aenean gravida egestas dolor eu lacinia. Nulla facilisi. Maecenas in imperdiet ante, at iaculis nunc. Vivamus eu elit quis metus malesuada feugiat. Duis mollis maximus orci, eget bibendum dui varius vitae. Aliquam eget pellentesque enim. Vivamus a erat nec est laoreet porttitor quis vel nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <p>Etiam ex nibh, venenatis at porttitor vitae, efficitur vitae odio. Donec consequat risus est, sed dapibus purus egestas quis. Ut quis varius tellus, at consectetur mi. Fusce ipsum enim, blandit ut risus vitae, congue gravida quam. Nam scelerisque diam a pulvinar ullamcorper. Ut vel mollis elit. Fusce eleifend lorem a ante mattis, efficitur interdum augue aliquet. Curabitur ut est blandit, commodo turpis viverra, tempor nisl. Duis in sollicitudin lorem. Aliquam ultricies lectus ex, eu suscipit risus elementum in. Vivamus eu elit vitae massa ultrices pellentesque sit amet fringilla enim. Fusce a viverra urna. Nam nibh quam, vehicula id laoreet non, feugiat eget arcu. Cras lobortis eleifend sollicitudin.</p>
+                </article>
+                
             </div>
     
         )
@@ -88,7 +100,7 @@ class About extends React.Component {
 class Projects extends React.Component {
     render = () => {
         return (
-            <div className="home-page">
+            <div className="projects-page">
                 <h1>Projects</h1>
             </div>
     
@@ -99,7 +111,7 @@ class Projects extends React.Component {
 class Current extends React.Component {
     render = () => {
         return (
-            <div className="home-page">
+            <div className="current-page">
                 <h1>Current</h1>
             </div>
     
